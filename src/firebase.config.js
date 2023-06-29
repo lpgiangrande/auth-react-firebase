b70a398e4d23b9fc1039706cc6efa35684e9b8d0
Laura G <62339671+lpgiangrande@users.noreply.github.com>
2023-06-29 15:16:55 +0200

    config firebase

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -6,12 +6,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAFw5WDBIZlriAqDHoSHB9uZxRImMJW8TY",
-  authDomain: "auth-react-8dc2b.firebaseapp.com",
-  projectId: "auth-react-8dc2b",
-  storageBucket: "auth-react-8dc2b.appspot.com",
-  messagingSenderId: "975179618711",
-  appId: "1:975179618711:web:930d3c5a6db56da0ef8cd2"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId:process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
